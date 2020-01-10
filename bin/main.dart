adaa7777d06e835de8e633fc4ed49c6f031f16f8
leejordan-git <leerjordan@protonmail.com>
2020-01-10 20:52:30 +0000

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dartium/dartium.dart' as dartium;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartium.calculate()}!');
+}
---
+++ test/dartium_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartium/dartium.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
